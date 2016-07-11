b7088f21f565b01e85b52f9443e59e08b5d06eaf
stardi <stardi@rdisoftware.com>
2016-07-11 10:46:05 +0200

    Datetimeformat

---
+++ Datetimeformat/Datetimeformat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetimeformat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = String.Format("xx{0:d MMMM yyyy HH:mm:ss}", DateTime.Now);
+        }
+    }
+}
