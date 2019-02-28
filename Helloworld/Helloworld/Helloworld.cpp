da23491b72405076b6f613565f5083872b19b97b
stardi <sandor.tardi@capgemini.com>
2019-02-28 17:57:43 +0000

    _ASSERT_EXPR(lkkt(0, 0) == 0, "0 0");

--- Helloworld/Helloworld/Helloworld.cpp
+++ Helloworld/Helloworld/Helloworld.cpp
@@ -7,9 +7,9 @@ int lkkt(int x, int y);
 int main()
 {
 	_ASSERT_EXPR(lkkt(5, 15) == 15, "5, 15");
-	//_ASSERT_EXPR(lkkt(15, 5) == 15, "15, 5");
-	//_ASSERT_EXPR(lkkt(20, 20) == 20, "20 20");
-	//_ASSERT_EXPR(lkkt(0, 0) == 0, "0 0");
+	_ASSERT_EXPR(lkkt(15, 5) == 15, "15, 5");
+	_ASSERT_EXPR(lkkt(20, 20) == 20, "20 20");
+	_ASSERT_EXPR(lkkt(0, 0) == 0, "0 0");
 	//_ASSERT_EXPR(lkkt(1, 1) == 1, "1 1");
 
 	return EXIT_SUCCESS;
@@ -19,6 +19,8 @@ int lkkt(int x, int y)
 {
 	int kt;
 	
+	if (x == 0 || y == 0) return 0;
+
 	for (kt = 1; kt < 10000000; ++kt) {
 		if (kt % x == 0 && kt % y == 0) 
 			return kt;
