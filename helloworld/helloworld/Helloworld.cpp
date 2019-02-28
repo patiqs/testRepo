3e0cdc1bc768a8b0739dd1db47daa182179769c5
futoflorian <futo97@gmail.com>
2019-02-28 18:29:41 +0100

    lkkt do while

---
+++ helloworld/helloworld/Helloworld.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include <iostream>
+
+int lnko(int x, int y);
+int lkkt(int x, int y);
+
+int main()
+{
+	int a, b;
+	int eredmeny;
+
+	scanf_s("%d %d", &a, &b);
+
+	eredmeny = lnko(a, b);
+	printf("%d\n", eredmeny);
+
+	eredmeny = lkkt(a, b);
+	printf("%d\n", eredmeny);
+
+
+	return EXIT_SUCCESS;
+}
+
+int lnko(int x, int y)
+{
+	while (x != y)
+	{
+		if (x > y)
+			x -= y;
+		else
+			y -= x;
+	}
+
+	return x;
+}
+
+int lkkt(int x, int y) 
+{
+	int kt = 0;
+
+	do 
+	{
+		kt++;
+	} while(kt < 100);
+
+	printf("%d\n", kt);
+
+	return kt;
+}
